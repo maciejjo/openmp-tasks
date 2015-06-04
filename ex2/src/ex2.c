6bfaa8bb1bd37c2ec14d971b46bba81896bc01e6
Maciej Sobkowski <maciejjo@maciejjo.pl>
2015-06-04 22:03:22 +0200

    Added affinity setting and RDTSC function for measuring elapsed cpu cycles

--- ex2/src/ex2.c
+++ ex2/src/ex2.c
@@ -1,3 +1,12 @@
+/*
+ *
+ * ex2
+ *
+ * A) Set affinity to single CPU for each OMP thread
+ * B) Prepare time measurment method using RDTSC x86 instruction
+ * 
+ */
+
 #define _GNU_SOURCE
 
 #include <stdio.h>
@@ -8,47 +17,69 @@
 #include <unistd.h>
 
 #include <omp.h>
+#include <pthread.h>
+ 
+/*
+ *
+ * From Intel® 64 and IA-32 Architectures Software Developer’s Manual:
+ *
+ * RDTSC — Read Time-Stamp Counter
+ *
+ * Opcode  Instruction  Op/En  64-Bit Mode  Compat/Leg Mode  Description
+ * 0F 31   RDTSC        NP     Valid        Valid            Read time-stamp counter into EDX:EAX.
+ * 
+ */
 
-struct dupa {
-
-	int member;
-	float liczba;
-	char znaczek;
-
-};
+static __inline__ unsigned long long rdtsc(void)
+{
+    unsigned long long int x;
+    __asm__ volatile (".byte 0x0f, 0x31" : "=A" (x));
+    return x;
+}
 
 int main()
 {
-	cpu_set_t cpuset;
-
-	struct dupa dupamiso;
-
 	printf("ex2\n");
-
 
 #pragma omp parallel
 	{
-		printf("Running in parallel...\n");
-		printf("My pid is %d\n", getpid());
-		printf("My thread num is %d\n", omp_get_thread_num());
+		pthread_t pt_handle;
+		int omp_thread_num;
+		int cpunum;
+		cpu_set_t cpuset;
+		int cpu_amount;
+		int i, j;
+		unsigned long long t1, t2;
+
+		pt_handle = pthread_self();
+		cpu_amount = CPU_COUNT(&cpuset);
+		omp_thread_num = omp_get_thread_num();
+		cpunum = omp_thread_num % cpu_amount;
+
+		CPU_ZERO(&cpuset);
+		CPU_SET(cpunum, &cpuset);
+		pthread_setaffinity_np(pt_handle, sizeof(cpuset), &cpuset);
+
+		CPU_ZERO(&cpuset);
+		pthread_getaffinity_np(pt_handle, sizeof(cpuset), &cpuset);
+
+		for(i = 0; i < cpu_amount; i++)
+			if(CPU_ISSET(i, &cpuset))
+				printf("[%d] I am running on CPU %d!\n", omp_thread_num, i);
+
+		j = 0;
+
+		t1 = rdtsc();
+
+		for(i = 0; i < 99999; i++)
+			j += 1;
+
+		t2 = rdtsc();
+
+		printf("Loop run for %llu cycles\n", t2 - t1);
+
 	}
 
-	printf("CPU_SETSIZE = %d\n", CPU_SETSIZE);
-
-	printf("Zeroing cpuset...\n");
-	CPU_ZERO(&cpuset);
-	printf("Done!\n");
-
-	printf("Getting CPU affinity...\n");
-	sched_getaffinity(getpid(), sizeof(cpuset), &cpuset);
-	printf("Done!\n");
-
-	printf("%d CPUs in cpuset.\n", CPU_COUNT(&cpuset));
-
-	dupamiso.member = 150;
-	dupamiso.znaczek = 'z';
-	dupamiso.liczba = 12312.312312;
-	
 	return EXIT_SUCCESS;
 
 }
