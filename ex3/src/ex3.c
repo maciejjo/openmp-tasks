c569552f57a0e5d51221f9aa803436d3c5f755da
Maciej Sobkowski <maciejjo@maciejjo.pl>
2015-06-17 22:40:55 +0200

    shared array method added

--- ex3/src/ex3.c
+++ ex3/src/ex3.c
@@ -15,7 +15,7 @@
 #include <omp.h>
 #include <pthread.h>
 
-#define NUM_STEPS 100000000L
+#define NUM_STEPS 10000L
 #define NUM_THREADS 4
  
 #if 0
@@ -53,7 +53,9 @@ int bind_omp_thread_to_one_cpu()
 	cpu_set_t cpuset;
 	unsigned int cpu_amount;
 
+	/*
 	unsigned int i;
+	*/
 	
 	omp_thread_num = omp_get_thread_num();
 	pt_handle = pthread_self();
@@ -62,13 +64,17 @@ int bind_omp_thread_to_one_cpu()
 	pthread_getaffinity_np(pt_handle, sizeof(cpuset), &cpuset);
 	cpu_amount = CPU_COUNT(&cpuset);
 
+	/*
 	printf("[%d] I run on %u cpus\n", omp_thread_num, cpu_amount);
+	*/
 
 	if(cpu_amount != 1) {
 
 		omp_thread_num = omp_get_thread_num();
 		cpunum = omp_thread_num % cpu_amount;
+		/*
 		printf("[%d] setting cpunum to %d\n", omp_thread_num, cpunum);
+		*/
 
 		CPU_ZERO(&cpuset);
 		CPU_SET(cpunum, &cpuset);
@@ -81,9 +87,11 @@ int bind_omp_thread_to_one_cpu()
 	}
 
 	pthread_getaffinity_np(pt_handle, sizeof(cpuset), &cpuset);
+	/*
 	for(i = 0; i < 4; i++)
 		if(CPU_ISSET(i, &cpuset))
 			printf("[%d] I am running on CPU %d!\n", omp_thread_num, i);
+	*/
 
 	return 0;
 
@@ -131,11 +139,6 @@ double compute_pi_parallel_v1(const unsigned long long num_steps, unsigned int n
 
 		bind_omp_thread_to_one_cpu();
 
-		printf("[%d] My pthread id = %lu\n", omp_get_thread_num(), pthread_self());
-
-		
-
-
 #pragma omp for
 
 		for(i = 0; i < num_steps; i++) {
@@ -179,8 +182,6 @@ double compute_pi_parallel_v2(const unsigned long long num_steps, unsigned int n
 
 		bind_omp_thread_to_one_cpu();
 
-		printf("[%d] My pthread id = %lu\n", omp_get_thread_num(), pthread_self());
-
 #pragma omp for reduction(+ : sum)
 
 		for(i = 0; i < num_steps; i++) {
@@ -193,6 +194,59 @@ double compute_pi_parallel_v2(const unsigned long long num_steps, unsigned int n
 			sum += 4.0f / (1.0f + (x * x));
 		}
 	}
+
+	return sum * step;
+
+}
+
+/* * *
+ * 
+ * compute_pi_parallel_v3
+ * 
+ * Zapewnić atomowość uaktualnienia współdzielonej sumy za pomocą #pragma omp
+ * atomic
+ *
+ * * */
+
+double compute_pi_parallel_v3(const unsigned long long num_steps, unsigned int num_threads)
+{
+
+	double x = 0.0f, sum = 0.0f;
+	volatile double *sum_partial = NULL;
+	const double step = 1.0f / num_steps;
+	unsigned int i;
+
+	omp_set_num_threads(num_threads);
+
+	if((sum_partial = calloc(num_threads, sizeof(float))) == NULL) {
+		perror("calloc");
+		exit(EXIT_FAILURE);
+	}
+
+#pragma omp parallel default(none) private(i, x) shared(sum_partial)
+	{
+
+		int omp_thread_num = 0;
+
+		bind_omp_thread_to_one_cpu();
+		omp_thread_num = omp_get_thread_num();
+
+#pragma omp for
+
+		for(i = 0; i < num_steps; i++) {
+
+			/*
+			printf("in step %d in thread %d\n", i, omp_thread_num);
+			*/
+
+			x = (i + 0.5f) * step;
+			sum_partial[omp_thread_num] += 4.0f / (1.0f + (x * x));
+		}
+
+	}
+
+	for(i = 0; i < num_threads; i++)
+		sum += sum_partial[i];
 
 	return sum * step;
 
@@ -219,6 +273,8 @@ int main()
 	double pi;
 	struct timespec ts_start, ts_end, ts_diff;
 
+	printf("========== PI SERIAL ==========\n");
+
 	if(clock_gettime(CLOCK_REALTIME, &ts_start) == -1) {
 		perror("clock gettime");
 		exit( EXIT_FAILURE );
@@ -237,6 +293,8 @@ int main()
 			ts_diff.tv_sec, ts_diff.tv_nsec);
 
 
+	printf("========== PI PARALLEL V1 ==========\n");
+
 	if(clock_gettime(CLOCK_REALTIME, &ts_start) == -1) {
 		perror("clock gettime");
 		exit( EXIT_FAILURE );
@@ -254,12 +312,35 @@ int main()
 	printf("Pi = %15.12f, computed in %ld.%09ld  seconds\n", pi,
 			ts_diff.tv_sec, ts_diff.tv_nsec);
 
+
+	printf("========== PI PARALLEL V2 ==========\n");
+
 	if(clock_gettime(CLOCK_REALTIME, &ts_start) == -1) {
 		perror("clock gettime");
 		exit( EXIT_FAILURE );
 	}
 
 	pi = compute_pi_parallel_v2(NUM_STEPS, NUM_THREADS);
+
+	if(clock_gettime(CLOCK_REALTIME, &ts_end) == -1) {
+		perror("clock gettime");
+		exit( EXIT_FAILURE );
+	}
+
+	ts_diff = get_timespec_diff(ts_start, ts_end);
+
+	printf("Pi = %15.12f, computed in %ld.%09ld  seconds\n", pi,
+			ts_diff.tv_sec, ts_diff.tv_nsec);
+
+
+	printf("========== PI PARALLEL V3 ==========\n");
+
+	if(clock_gettime(CLOCK_REALTIME, &ts_start) == -1) {
+		perror("clock gettime");
+		exit( EXIT_FAILURE );
+	}
+
+	pi = compute_pi_parallel_v3(NUM_STEPS, NUM_THREADS);
 
 	if(clock_gettime(CLOCK_REALTIME, &ts_end) == -1) {
 		perror("clock gettime");
